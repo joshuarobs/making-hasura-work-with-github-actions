03651070b33d0421b0693cbe6004236146890a99
joshuarobs <52946881+joshuarobs@users.noreply.github.com>
2020-01-07 18:59:00 +1100

    Create docker-run.sh

---
+++ docker-run.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+docker run -d --net=host \
+       -e HASURA_GRAPHQL_DATABASE_URL=postgres://admin:@localhost:5432/my-db \
+       -e HASURA_GRAPHQL_ENABLE_CONSOLE=true \
+       hasura/graphql-engine:v1.0.0
