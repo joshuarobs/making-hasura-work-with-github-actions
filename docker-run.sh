fc192fecde1f5505da2cc6106658360d6d8c13d6
Joshua Robillos <joshuarobs@icloud.com>
2020-01-07 19:12:00 +1100

    Commit

--- docker-run.sh
+++ docker-run.sh
@@ -2,4 +2,5 @@
 docker run -d --net=host \
        -e HASURA_GRAPHQL_DATABASE_URL=postgres://admin:@localhost:5432/my-db \
        -e HASURA_GRAPHQL_ENABLE_CONSOLE=true \
+       --name myserver \
        hasura/graphql-engine:v1.0.0
