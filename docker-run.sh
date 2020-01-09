221f95b7ff04ab068348c0f00af0995057af63ac
joshuarobs <52946881+joshuarobs@users.noreply.github.com>
2020-01-09 22:55:50 +1100

    Delete docker-run.sh

--- docker-run.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-docker run -d --net=host \
-       -e HASURA_GRAPHQL_DATABASE_URL=postgres://postgres:@postgres:5432/postgres \
-       -e HASURA_GRAPHQL_ENABLE_CONSOLE=true \
-       --name myserver \
-       hasura/graphql-engine:v1.0.0
