9ef1bbfdbc6ca35d537850509cf6b061d95321ea
joshuarobs <52946881+joshuarobs@users.noreply.github.com>
2020-01-09 21:46:53 +1100

    Update docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 docker run -d --net=host \
-       -e HASURA_GRAPHQL_DATABASE_URL=postgres://admin:@localhost:5432/my-db \
+       -e HASURA_GRAPHQL_DATABASE_URL=postgres://postgres:@localhost:5432/my-db \
        -e HASURA_GRAPHQL_ENABLE_CONSOLE=true \
        --name myserver \
        hasura/graphql-engine:v1.0.0
